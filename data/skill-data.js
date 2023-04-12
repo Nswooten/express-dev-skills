46daab68003baeb5cc6e42c83d67384cf2fbf634
Nick Wooten <nswooten@gmail.com>
2023-04-11 22:12:58 -0500

    Complete part 1

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+    {text: "html", done: true, _id: 123456},
+    {text: "css", done: false, _id: 123456},
+    {text: "javascript", done: false, _id: 123456},
+]
+
+export {
+    skills
+}
