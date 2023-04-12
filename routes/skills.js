b2b20296918f3a8142181647221c3cb515190bf0
Nick Wooten <nswooten@gmail.com>
2023-04-12 18:06:18 -0500

    finish part 3

--- routes/skills.js
+++ routes/skills.js
@@ -7,5 +7,11 @@ const router = Router()
 // GET localhost:3000/users
 // GET localhost:3000/todos
 router.get('/', skillsCtrl.index)
+router.get("/new", skillsCtrl.new)
+router.get("/:skillId", skillsCtrl.show)
+
+
+
+router.post("/", skillsCtrl.create)
 
 export { router }
